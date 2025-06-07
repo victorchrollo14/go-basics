01951f6f924be59e97edd3dd3485ee3b40897472
victorchrollo14 <chrollolucilfer1402@gmail.com>
2025-06-07 14:02:02 +0530

    add case to delete book from library"

--- basic_programs/library/main.go
+++ basic_programs/library/main.go
@@ -12,6 +12,35 @@ import (
 // Design a program that models a library with books and authors using structs.
 // Create a map where keys are author names and values are slices of book structs.
 // Implement functions to add books and search for books by author or title.
+func SearchList(bookList []Book, id string) (Book, bool) {
+	for _, val := range bookList {
+		if val.id == id {
+			return val, true
+		}
+	}
+
+	return Book{}, false
+}
+
+// returns the index of the search item
+func SearchIndex(bookList []Book, id string) int {
+	for i, val := range bookList {
+		if val.id == id {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func LogBooks(bookList []Book) {
+	for _, val := range bookList {
+		fmt.Printf("{id: '%v', author: '%v', title: '%v', genre: '%v' }\n", val.id, val.author, val.title, val.genre)
+	}
+	fmt.Println("\n\n")
+
+}
+
 func main() {
 	// initialAuthor := AuthorList;
 	initialBooks := BookList
@@ -47,38 +76,51 @@ func main() {
 		case 5:
 			fmt.Println("quiting...")
 			os.Exit(0)
+
 		case 1:
 			fmt.Println("\nThe Books list: ")
-			for _, val := range initialBooks {
-				fmt.Printf("{id: '%v', author: '%v', title: '%v', genre: '%v' }\n", val.id, val.author, val.title, val.genre)
-			}
-			fmt.Println("\n\n")
+      LogBooks(initialBooks)
+
 		case 2:
 			fmt.Println("Creating an new book...")
 
 			id := fmt.Sprintf("B00%v", len(initialBooks)+1)
 
-			reader := bufio.NewReader(os.Stdin)
-
 			fmt.Println("Enter book details")
 			fmt.Print("Name of the book: ")
 			title, _ := reader.ReadString('\n')
-      title = strings.TrimSpace(title)
+			title = strings.TrimSpace(title)
 
 			fmt.Print("Author of the book: ")
 			author, _ := reader.ReadString('\n')
-      author = strings.TrimSpace(author)
+			author = strings.TrimSpace(author)
 
 			fmt.Print("Genre of the book: ")
 			genre, _ := reader.ReadString('\n')
-      genre = strings.TrimSpace(genre)
+			genre = strings.TrimSpace(genre)
 
-			initialBooks = append(initialBooks, Book{id, author, title, genre})
-			fmt.Println("\nThe New Books list: ")
-			for _, val := range initialBooks {
-				fmt.Printf("{id: '%v', author: '%v', title: '%v', genre: '%v' }\n", val.id, val.author, val.title, val.genre)
+			initialBooks = append(initialBooks, Book{id, author, title, genre})	
+      LogBooks(initialBooks)
+
+		case 3:
+			fmt.Println("Deleting book...")
+			fmt.Println("Enter the book id: ")
+			delId, _ := reader.ReadString('\n')
+			delId = strings.TrimSpace(delId)
+
+			book, found := SearchList(initialBooks, delId)
+			if found == false {
+				fmt.Print("Didn't find any book with the id: %v, try other id\n", delId)
+				continue
 			}
-			fmt.Println("\n\n")
+
+			fmt.Println("the book to be deleted is ", book)
+			index := SearchIndex(initialBooks, delId)
+			fmt.Println("the index of book to be deleted is ", index)
+			initialBooks = append(initialBooks[:index], initialBooks[index+1:]...)
+			fmt.Println("the library after deleting a book")
+      LogBooks(initialBooks)
+
 		default:
 			fmt.Printf("Option %v doesn't exist\n", optNum)
 
